603c6806de02d5d8035a00f0acae5e236acae26b
haoziapple <haozixiaowang@163.com>
2019-08-28 16:47:58 +0800

    nacos-provider使用nacos作为配置中心，注意：1.dataId的组成 2. 配置中心相关配置、spring.application.name必须配置在bootstrap里
    Signed-off-by: haoziapple <haozixiaowang@163.com>

--- nacos-provider/src/main/java/haozi/lab/nacos/provider/ProviderController.java
+++ nacos-provider/src/main/java/haozi/lab/nacos/provider/ProviderController.java
@@ -2,6 +2,8 @@ package haozi.lab.nacos.provider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019-08-28 14:10
  */
 @RestController
+@RefreshScope
 public class ProviderController {
     Logger logger = LoggerFactory.getLogger(ProviderController.class);
+
+    /**
+     * 似乎会注入系统变量：Administrator
+     */
+    @Value("${username}")
+    private String username;
+
+    @Value("${name:wanghao}")
+    private String name;
 
     @GetMapping("/hi")
     public String hi(@RequestParam(value = "name", defaultValue = "forezp", required = false) String name) {
         return "hi, this is nacos-provider: " + name;
+    }
+
+    @GetMapping
+    public String getName() {
+        return name;
     }
 }
